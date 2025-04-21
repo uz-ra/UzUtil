442e48b955c6bb6f79c07d587cf5cf461227070a
uz.ra <tanoura.uz@gmail.com>
2025-04-21 12:14:07 +0900

    Update file name and etc

--- UzFileManager/UzFileManager.h
+++ UzFileManager/UzFileManager.h
@@ -3,6 +3,6 @@
 @interface UzFileManager : NSObject
 + (NSString *)dataDirForBundleID:(NSString *)bundleID;
 + (NSString *)bundleDirForBundleID:(NSString *)bundleID;
-+ (void)overwriteXML:(NSString *)inPath : (NSString *)forKey : (NSObject)withData;
++ (void)overwriteXML:(NSString *)inPath : (NSString *)forKey : (id)withData;
 + (void)overwriteJSON:(NSString *)inPath : (NSString *)keyPath : (id)withData;
 @end
